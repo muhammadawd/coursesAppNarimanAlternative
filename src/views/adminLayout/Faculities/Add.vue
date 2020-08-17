ec5ba7ce8d95191fa8b197183b7846151274f232
unknown <devmohamedawd@gmail.com>
2020-08-17 14:56:43 +0200

    fauclty view

--- src/views/adminLayout/Faculities/Add.vue
+++ src/views/adminLayout/Faculities/Add.vue
@@ -78,9 +78,6 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-12">
-                                        <hr>
-                                    </div>
                                 </div>
                             </div>
                         </div>
