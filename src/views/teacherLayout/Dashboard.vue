ed25f7553d42673829ca73eff1c99e01d4ca7546
unknown <devmohamedawd@gmail.com>
2020-08-18 14:20:16 +0200

    new pages

--- src/views/teacherLayout/Dashboard.vue
+++ src/views/teacherLayout/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <u-animate-container>
-            <div class="row">
+            <div class="row mt-2">
                 <div class="col-md-8">
                     <div class="card">
                         <div class="card-header border-bottom-1">
